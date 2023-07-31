e8c097ba4e495bcf75b46c740b67984fd5538405
Abhishek Mishra <mishraavi790@gmail.com>
2023-07-31 15:14:11 +0545

    query builder

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     @forelse($tasks as $task)
         <div>
             {{-- in route we pass route name and also parameter --}}
-            <a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a>
+            <a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task['title']}}</a>
         </div>
     @empty
         <div>There are no tasks!</div>
--- routes/web.php
+++ routes/web.php
@@ -4,79 +4,24 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 
-
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        // this ? makes it optional
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks,
+        // this method here is starting a thing called query builder,which lets you build queries
+        'tasks' => \App\Models\Task::latest()->get()
 
     ]);
+
 })->name('tasks.index');
 
 Route::get('/tasks/{id}', function ($id) {
-    // lets you fetch a record form the database one single row by its primary key
-    \App\Models\Task::find();
+    // lets you fetch a record form the database one single row by its primary key,if it cannot find the record it will call the abort function with 404 status code
 
-
-    return view('show');
+    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
 
